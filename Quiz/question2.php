9c2c6c04c43bdad807a95089d058725c1add5fb6
pedro-6969 <pedromiguel19712@gmail.com>
2025-07-29 16:22:34 -0300

    as

--- Quiz/question2.php
+++ Quiz/question2.php
@@ -9,10 +9,11 @@
     <link rel="stylesheet" href="style.css">
   </head>
   <body>
-    
-    <h1>Hello, world!</h1>
 
-    <a href="./user.php">Iniciar teste de QI</a>
+    <?php
+        echo "<pre>";
+        echo var_dump($_SESSION);
+    ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js" integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q" crossorigin="anonymous"></script>
   </body>
