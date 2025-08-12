47f77dc7727b33b40449fa8bb636b12db89a1f9b
pedro-6969 <pedromiguel19712@gmail.com>
2025-08-12 15:54:17 -0300

    a

---
+++ CRUD_PDO_POO/avaliacao.php
@@ -0,0 +1,23 @@
+<?php
+    class Editora {
+        private $id;
+        private $nota;
+        private $comentario;
+        private $data;
+
+        public function __construct($nota, $comentario, $data){
+            $this->nota = $nota;
+            $this->comentario = $comentario;
+            $this->data = $data;
+        }
+        public function inserir(){}
+
+        public function buscar($id){}
+
+        public function buscarTodos(){}
+
+        public function atualizar($id){}
+
+        public function deletar($id){}
+    }
+?>
---
+++ CRUD_PDO_POO/categoria.php
@@ -0,0 +1,19 @@
+<?php
+    class Categoria {
+        private $id;
+        private $nome;
+
+        public function __construct($nome){
+            $this->nome = $nome;
+        }
+        public function inserir(){}
+
+        public function buscar($id){}
+
+        public function buscarTodos(){}
+
+        public function atualizar($id){}
+
+        public function deletar($id){}
+    }
+?>
